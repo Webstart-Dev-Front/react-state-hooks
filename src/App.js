e8f3cefde8b67b31c5ccfe7f23ac31aa0aa251be
Nezzar KEFIF <kefifnezzar@gmail.com>
2020-11-03 16:35:00 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
+// GÈRER LE STATE (l'Etat d'un composant react).
+// Créer un projet react:
+// - Créer un nouveau répo dans l'organisation github: react-state-[VOTRE_PRENOM]
+// - Créer un nouveau projet react: npx create-react-app react-state
+
+// JSS
+
+// Ternaire
+
+// Inserer du JS dans le JSX
+
+// Les fonctions dans les composant
+
+// function add() {
+
+// }
+
+// Arrow functions
+
+// const add = () => {
+
+// }
+
+// Créer deux boutons + et - qui incremente et décrémente une variable number initialisé via un hook (useState)
+// Ajouter une augmentation de taille lorsqu'on incremente
+
 function App() {
+  // Un hook
+  const [red, setRed] = useState(false)
+
+  const changeToRed = () => {
+    setRed(true)
+  }
+
+  const changeToBlue = () => {
+    setRed(false)
+  }
+
+  // JSX
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={changeToRed}>Changer en rouge</button>
+      <button onClick={changeToBlue}>Changer en bleu</button>
+      <p
+        style={{
+          color: red ? 'red' : 'blue'
+        }}
+      >{red ? 'Rouge' : 'Pas rouge'}</p>
     </div>
   );
 }
